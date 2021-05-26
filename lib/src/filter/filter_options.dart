0815828d38d0e29338dc279b084b47bd7d02c9c6
Rafał Płonka <rafi.apollo@gmail.com>
2021-05-26 10:05:21 +0200

    Update filter_options.dart

--- lib/src/filter/filter_options.dart
+++ lib/src/filter/filter_options.dart
@@ -9,7 +9,7 @@ import '../type.dart';
 /// See [FilterOption]
 class FilterOptionGroup {
   static final _defaultOrderOption = OrderOption(
-    type: OrderOptionType.updateDate,
+    type: OrderOptionType.createDate,
     asc: false,
   );
 
